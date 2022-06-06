4a7e7451dc87e51f803a13da071ada84696fed9c
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-07 00:23:48 +0900

    コンポーネントでレイアウト作成

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
-import Header from "./header";
-import Link from "next/link";
+import Layout from "../components/layout";
+import MyImage from "../components/image";
 
 export default function Home() {
-  const title = "Index";
-
   return (
     <div>
-      <Header title={title} />
-      <h1 className="bg-primary px-3 text-white display-4">React</h1>
-      <div className="container">
-        <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これは、ページ移動のサンプルです。</p>
-          <Link href="/other">
-            <a>Go to Other page &gt;&gt;</a>
-          </Link>
+      <Layout header="Next.js" title="Top page.">
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">Welcome to next.js!</h5>
+          <MyImage fname="image.jpg" size="300" />
         </div>
-      </div>
+      </Layout>
     </div>
   );
 }
