2c85707032a34bd25bf0de76e2593752b47b8390
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-07 11:40:45 +0900

    ~5.3

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,40 @@
 import { useState } from "react";
 import Layout from "../components/layout";
+import useSWR from "swr";
 
 export default function Home() {
-  const localhost = "http://localhost:3000";
-  const url = "/data.json";
+  const [pref, setPref] = useState({ id: 0, item: "name" });
+  const [address, setAddress] = useState(
+    "/api/hello/" + pref.id + "/" + pref.item
+  );
+  const { data, err } = useSWR(address);
 
-  const [data, setData] = useState({ message: "", data: [] });
-
-  fetch(localhost + url)
-    .then((res) => res.json())
-    .then((res) => setData(res));
+  const onChange = (e) => {
+    pref.id = e.target.value;
+    setPref(pref);
+    setAddress("/api/hello/" + pref.id + "/" + pref.item);
+  };
+  const onSelect = (e) => {
+    pref.item = e.target.value;
+    setPref(pref);
+    setAddress("/api/hello/" + pref.id + "/" + pref.item);
+  };
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data.message}</h5>
-          <table className="table bg-white">
-            <thead className="table-dark">
-              <tr>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.data.map((value, key) => (
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <td>{value.mail}</td>
-                  <td>{value.age}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <h5 className="mb-4">{JSON.stringify(data)}</h5>
+          <input
+            type="number"
+            className="form-control form-control-sm mb-2"
+            onChange={onChange}
+          />
+          <select onChange={onSelect} className="form-control form-control-sm">
+            <option value="name">Name</option>
+            <option value="mail">Mail</option>
+            <option value="age">Age</option>
+          </select>
         </div>
       </Layout>
     </div>
