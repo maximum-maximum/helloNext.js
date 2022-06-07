f01c15154d094d9ba70fe8834d944a54a3b2fa7e
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-07 10:48:49 +0900

    crossorigin -> crossOrigin

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout(props) {
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
       </Head>
       <Header header={props.header} />
--- pages/header.js
+++ pages/header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
       <link
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></link>
     </Head>
   );
